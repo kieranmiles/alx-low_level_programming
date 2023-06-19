119e409930b7d5d31b8a77a36be8e15b817600d0
kieranmiles <heranisaac7th@gmail.com>
2023-06-19 03:46:13 -0700

    102 print

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -2,42 +2,31 @@
 #include <stdlib.h>
 
 /**
- * main - Entry point.
+ * main - Entry point
  *
  * Return: Always 0 (Success)
  */
 
 int main(void)
 {
-	int i, j, k, l;
+        int i, j;
 
-	for (i = 0; i <= 9; i++)
-	{
-		for (j = 0; j <= 9; j++)
-		{
-			for (k = 0; k <= 9; k++)
-			{
-				for (l = 0; l <= 9; l++)
-				{
-					if (i == j && j == k && k == l)
-					{
-						continue;
-					}
-					putchar(i + '0');
-					putchar(j + '0');
-					putchar(' ');
-					putchar(k + '0');
-					putchar(l + '0');
-					if (i == 9 && j == 9 && k == 9 && l == 9)
-					{
-						break;
-					}
-					putchar(',');
-					putchar(' ');
-				}
-			}
-		}
-	}
-	putchar('\n');
-	return (0);
+        for (i = 0; i < 100; i++)
+        {
+                for (j = i + 1; j < 100; j++)
+                {
+                        putchar((i / 10) + '0');
+                        putchar((i % 10) + '0');
+                        putchar(' ');
+                        putchar((j / 10) + '0');
+                        putchar((j % 10) + '0');
+                        if (i != 98 || j != 99)
+                        {
+                                putchar(',');
+                                putchar(' ');
+                        }
+                }
+        }
+        putchar('\n');
+        return (0);
 }
