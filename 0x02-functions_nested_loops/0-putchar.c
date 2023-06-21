64dcf7a26994bfd50a5f09d4eb6506357d36ab03
kieranmiles <heranisaac7th@gmail.com>
2023-06-21 03:00:26 -0700

    putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - print _putchar
+ *witten by kieran
+ * return: 0 (successful)
+ */
+
+int main (void)
+{
+	printf 
+		_putchar('_');
+		_putchar('p');
+		_putchar('u');
+		_putchar('t');
+		_putchar('c');
+		_putchar('h');
+		_putchar('a');
+		_putchar('r');
+		_putchar('\n');
+		return (0);
+}
