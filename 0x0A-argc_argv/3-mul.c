c65fb857c5d2564e95ca7e61410628b61fd61af7
kieranmiles <heranisaac7th@gmail.com>
2023-07-07 11:22:29 -0700

    3 mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -16,30 +16,30 @@ int _atoi(char *s)
 	len = 0;
 	f = 0;
 	digit = 0;
+
 	while (s[len] != '\0')
 		len++;
-
 	while (i < len && f == 0)
 	{
 		if (s[i] == '-')
 			++d;
-
 		if (s[i] >= '0' && s[i] <= '9')
 		{
 			digit = s[i] - '0';
-
 			if (d % 2)
+
 				digit = -digit;
+
 			n = n * 10 + digit;
 			f = 1;
-
 			if (s[i + 1] < '0' || s[i + 1] > '9')
 
 				break;
-			f = 0;
 
+			f = 0;
 		}
 		i++;
+
 	}
 
 	if (f == 0)
@@ -60,11 +60,14 @@ int _atoi(char *s)
 int main(int argc, char *argv[])
 {
 	int result, num1, num2;
+
 	if (argc < 3 || argc > 3)
+
 	{
 		printf("Error\n");
 
 		return (1);
+
 	}
 
 	num1 = _atoi(argv[1]);
